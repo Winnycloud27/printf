8ee125519ff2d5223ebb40674d333a793d0d4bf9
Winnycloud27 <winnyezinne27@gmail.com>
2023-12-04 10:36:00 +0100

    Header file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+
+/* The main printf prototype */
+int _printf(const char *format, ...);
+
+#endif
+
